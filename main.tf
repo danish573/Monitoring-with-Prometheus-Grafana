fb5a5642d55394427752c4bb832ba80daa0817e9
danish573 <dkhan573@gmail.com>
2025-08-03 12:28:16 +0530

    Code Update

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+<<<<<<< HEAD
+=======
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+>>>>>>> e482348 (Code Update)
 provider "aws" {
   region = var.aws_region
 }
@@ -9,39 +23,63 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+<<<<<<< HEAD
     cidr_blocks = ["0.0.0/0"]
+=======
+    cidr_blocks = ["0.0.0.0/0"]
+>>>>>>> e482348 (Code Update)
   }
 
   ingress {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
+<<<<<<< HEAD
     cidr_blocks = ["0.0.0/0"]
+=======
+    cidr_blocks = ["0.0.0.0/0"]
+>>>>>>> e482348 (Code Update)
   }
 
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
+<<<<<<< HEAD
     cidr_blocks = ["0.0.0/0"]
+=======
+    cidr_blocks = ["0.0.0.0/0"]
+>>>>>>> e482348 (Code Update)
   }
   ingress {
     from_port   = 9100
     to_port     = 9100
     protocol    = "tcp"
+<<<<<<< HEAD
     cidr_blocks = ["0.0.0/0"]
+=======
+    cidr_blocks = ["0.0.0.0/0"]
+>>>>>>> e482348 (Code Update)
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+<<<<<<< HEAD
     cidr_blocks = ["0.0.0/0"]
+=======
+    cidr_blocks = ["0.0.0.0/0"]
+>>>>>>> e482348 (Code Update)
   }
 
 }
 
+<<<<<<< HEAD
 resource "aws-instance" "prometheus_grafana_instance" {
+=======
+resource "aws_instance" "prometheus_grafana_instance" {
+>>>>>>> e482348 (Code Update)
   ami             = var.ami_id
   instance_type   = var.instance_type
   key_name        = var.key_name
--- variable.tf
+++ variable.tf
@@ -14,11 +14,20 @@ variable "instance_type" {
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instance"
   type        = string
+<<<<<<< HEAD
   default     = "ami-0c55b159cbfafe1f0" # Example AMI ID, replace with a valid one for your region
+=======
+  default     = "ami-020cba7c55df1f615" # Example AMI ID, replace with a valid one for your region
+>>>>>>> e482348 (Code Update)
 }
 
 variable "key_name" {
   description = "The name of the SSH key pair to use for accessing the EC2 instance"
+<<<<<<< HEAD
   type        = string
   default     = "my-key-pair" # Replace with your actual key pair name
+=======
+    type        = string
+    default = "Linux1"
+>>>>>>> e482348 (Code Update)
 }
